5b3bd4d34f8099c3b459101ead3c827ca7bdf7e9
erictomson <erictomson@gmail.com>
2021-09-09 12:20:20 -0300

    Controlando as taxas de reajuste dentro do laço For

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -13,37 +13,49 @@ public class Funcionario {
         int anoInicial = 1995;
         //Ano Final = Ano Atual (2021)
         LocalDate anoFinal = LocalDate.now();
+        int anoAtual;
 
         //Definindo o tamanho do array a partir dos anos que quero trabalhar
         Double[] salarios = new Double[(anoFinal.getYear()-anoInicial)+1];
         //Controlando a taxa de reajuste e salário atualizado
-        Double reajuste;
+        Double reajuste=0.0;
         Double salarioAtual=0.0;
 
-        //Definindo o salário inicial
-        //Esse funcionário foi contratado em 1995, com o salário inicial de R$1000,00;
-        salarios[0]=1000.00;
-
-        //Em 1996 recebeu um aumento de 1,5% sobre o seu salário inicial;
-        reajuste=0.015; //1,5%
-        salarios[1]=salarios[0]*(1+reajuste);
+        //Inicializando o controle do anoAtual (1995)
+        anoAtual=anoInicial;
 
         //A partir de 1997 (inclusive), os aumentos salariais sempre corresponderam ao
         //dobro do percentual do ano anterior.
-        for(int i=2;i<salarios.length;i++) {
-            //dobrar o percentual
-            reajuste=reajuste*2;
+        for(int i=0;i<salarios.length;i++) {
+            if(anoAtual==1995) {
+                //Definindo o salário inicial
+                //Esse funcionário foi contratado em 1995, com o salário inicial de R$1000,00;
+                salarios[i]=1000.00;
+                anoAtual++;
+                continue;
+            } else if (anoAtual==1996) {
+                //Em 1996 recebeu um aumento de 1,5% sobre o seu salário inicial;
+                reajuste=0.015; //1,5%
+            } else {
+                // para os outros anos, dobrar o percentual de reajuste
+                reajuste = reajuste * 2;
+            }
             //atualizar o salário do ano, aplicando a nova taxa de reajuste
             //sobre o salário anterior
             salarios[i]=salarios[i-1]*(1+reajuste);
             //atualizar o salário atual para exibir para o usuário
             salarioAtual=salarios[i];
+            //Incrementa o ano atual
+            anoAtual++;
         }
+
+        //Reinicializando o controle do anoAtual (1995)
+        anoAtual=anoInicial;
 
         // Imprimir todos os salários
         for(int i=0;i<salarios.length;i++) {
-            System.out.println("Salário ano " + anoInicial + ": " + NumberFormat.getCurrencyInstance().format(salarios[i]));
-            anoInicial++;
+            System.out.println("Salário ano " + anoAtual + ": " + NumberFormat.getCurrencyInstance().format(salarios[i]));
+            anoAtual++;
         }
 
         System.out.println("Salário atual: " + NumberFormat.getCurrencyInstance().format(salarioAtual));
